4e9b690c936a16eb1882f03a0256c846b8c58176
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-12-04 00:28:33 -0500

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -4,12 +4,12 @@ while true; do
     n=1
     for var in `seq 1 $n`;
     do
-	ip=`cat /ip.txt | shuf -n 1`
-	ts=`date +"[%d/%b/%Y:%k:%M:%S %z]"`
-	uri=`cat /words.txt | shuf -n 1`
-	status=`cat /status.txt | shuf -n 1`
-	echo "${ip} - - ${ts} \"GET /${uri} HTTP/1.0\" ${status} 1702"
+        ip=`cat $BUILD_DIR/ip.txt | shuf -n 1`
+        ts=`date +"[%d/%b/%Y:%k:%M:%S %z]"`
+        uri=`cat $BUILD_DIR/words.txt | shuf -n 1`
+        status=`cat $BUILD_DIR/status.txt | shuf -n 1`
+        echo "${ip} - - ${ts} \"GET /${uri} HTTP/1.0\" ${status} 1702"
 
     done
-    sleep 60;
+    sleep 15;
 done
